fdbc8cc33672b2735a689da29cda6d50a73fbe37
RYA234 <103587568+RYA234@users.noreply.github.com>
2022-12-18 03:14:53 +0000

    簡易レイアウト構築

--- ecshop_front/pages/index.tsx
+++ ecshop_front/pages/index.tsx
@@ -6,28 +6,89 @@ import Left from '../component/left';
 import Sidebar from '../component/left';
 import Right from '../component/right';
 
-
-
 export default function Home() {
 	// ApiFetch();
 	return (
-		<>
-			<Header left={0} top={0} color='yellow' bgColor='green' width={1440} height={95} />
-			{/* <Sidebar  left={0} top={95} color='black' bgColor='springgreen' width={212} height={862}/> */}
-
+		<div>
+			<header>ヘッダー </header>
 			<main>
-			<Left left={0} top={100} color='yellow' bgColor='gray' width={200} height={1000} />
-			<Center left={212} top={95} color='black' bgColor='rgba(252, 154, 154, 0.39)' width={1150} height={862} />
-			<Right left={1370} top={100} color='black' bgColor='yellow' width={300} height={1000} />
+				<div>
+					<div className="left">
+						左側
+
+					</div>
+
+
+					<div className='center'>
+						{/* <Left left={0} top={100} color='yellow' bgColor='gray' width={200} height={1000} /> */}
+						{/* <Center left={212} top={95} color='black' bgColor='rgba(252, 154, 154, 0.39)' width={1150} height={862} /> */}
+						{/* <Right left={1370} top={100} color='black' bgColor='yellow' width={300} height={1000} /> */}
+						センター
+						<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+						<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+						<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+						<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+						<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+
+						終わり
+					</div>
+
+					<div className="right">
+						右側
+
+					</div>
+				</div>
+
 			</main>
+			<footer>フッター</footer>
 
 			{/* Number型は{}で囲みString型は''で囲む下参照 */}
 			{/* <Footer left={0} top={957} color='yellow' bgColor='gray' width={1440} height={59} /> */}
-		
-		</>
+
+			<style jsx>{`
+				header{
+					position:sticky;
+					top: 0;
+					left: 0;
+					//width: 100%;
+					height: 120px;
+					color:black;
+					background:yellow;
+				}
+				footer{
+					color:yellow;
+					background:purple;
+					height:80px;
+					bottom:0;
+					position:absolute;
+					width:100%;
+				}
+				main{
+					flex-grow: 1;
+					background:pink;
+				}
+				.leftCenterRightInMain{
+					display: flex;
+					flex-direction: row;
+				}
+				.left{
+					flex:1;
+					color:rainbow;
+					background:pink;
+				}
+				.center{
+					flex:3;
+					background:white;
+				}
+				.right{
+					flex:1;
+					background:red;
+				}
+			`}</style>
+		</div>
 	)
 };
-
 
 // restapi 検証用の関数。
 export const ApiFetch = () => {
