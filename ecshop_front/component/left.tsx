13d5ea93a7060cac5f7fc44e7b8730832957f5ad
RYA234 <103587568+RYA234@users.noreply.github.com>
2022-12-21 13:53:32 +0000

    とりあえず動くようにした　todo　リンクを実装する

--- ecshop_front/component/left.tsx
+++ ecshop_front/component/left.tsx
@@ -7,7 +7,8 @@ import { useEffect, useState } from "react";
 // 　　左サブメニューのコンポーネント
 export default function Left(this: any, { left, top, color, bgColor, height, width }: any) {
 	const [categories, setCategories] = useState([])
-	const [visible, setVisible] = useState("hidden");
+
+
 	useEffect(() => {
 		// fetch("http://localhost:5000/api/category/all", { method: "GET" })
 		fetch("http://my-json-server.typicode.com/RYA234/ecshop_front_react/categories", { method: "GET" })
@@ -15,79 +16,92 @@ export default function Left(this: any, { left, top, color, bgColor, height, wid
 			.then((res) => res.json())
 			.then(data => {
 				setCategories(data);
-				console.log(data);
+				//console.log(data);
 			})
 	}, [])
 
-	function onMouse(e){
-		setVisible("visible");
-	}
-
-	function onMouseOut(e){
-		setVisible("hidden");
-	}
 
 	return (
 		<div>
 			カテゴリー覧
+			<nav>
 			<ul>
 				{
 					categories.map(category => {
 						if (category.parent == null)
 							return (
 								// eslint-disable-next-line react/jsx-key
-								<li>
-									<div className='mainMenu' onMouseEnter={onMouse} onMouseOut={onMouseOut} >{category.name}</div>
-									<div id={category.name}  className='subMenu'>
+								<li className='mainMenu'>
+									<a href="#">{category.name}</a>					
+									{/* <div id={category.name}  className='subMenu'> */}
+									<ul>
 										{
 											 category.children.map(small => {
 												const  title  = categories.at(parseInt(small)).name;
-												console.log(title);
+												//console.log(title);
 												return(
 													// <div>{child}</div>
 													// eslint-disable-next-line react/jsx-key
-													<a href="uhii">{title}<br/></a>			
+													<li><a href="uhii">{title}<br/></a></li>			
 												)
 											 })
 										}
-									</div>
+									</ul>
 									
 								</li>
 							);
 					})
 				}
 			</ul>
-
+			</nav>
 			<>
 
 			</>
-			<style jsx>{`
-				div{
-					// position:fixed;
-					// left:${left}px;
-					// top:${top}px;
-					// width:${width}px;
-					//height:${height}px;
-					// height:100%;
-					// color:${color};
-					// background:${bgColor};	
-				}
-				.mainMenu{
-					left:100px;
-					//position:relative;
-					list-style: none;
+			<style jsx>{`	
+				nav ul{		
 					visibility: visible;
-					margin: 8px 0 0 12px;
-					
+					list-style: none;
 				}
-				.subMenu{
+				nav ui li{
+					position: relative;
+				}
+				nav ul li ul{
 					position:absolute;
-					margin: -20px 0 0 90px;
-					padding: 0 0 0 12px;
-					z-index:4;
-					visibility: ${visible};
+					z-index:6;
+					visibility: hidden;
+					 list-style: none;
+					padding: 0px;
+					top:130px;
+					width:180px;
 				}
-			
+				
+				nav li.mainMenu ul
+				{
+					position: absolute;
+					left:100%;
+					top:10px;
+					z-index: 6;
+					width:180px;
+					  /*はじめは非表示*/
+					visibility: hidden;
+				}
+
+				nav  li.mainMenu:hover > ul,
+				nav  li.mainMenu ul li:hover > ul,
+				nav  li.mainMenu ul li:active > ul,
+				nav  li.mainMenu:active > ul
+				{
+					position:absolute;
+					 z-index:6;
+					visibility: visible;
+					list-style: none;
+					padding: 20px;
+				    margin: 20px 10px 100px 0px;
+					top:100px;
+					//　leftの自動調整方法　このやり方だと、サブメニューの幅が変わるとずれる　
+				    left:10%;
+					width:180px;					
+				}
 			`}</style>
 		</div>
 	)
