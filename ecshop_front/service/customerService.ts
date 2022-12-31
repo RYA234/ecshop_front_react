e0c275a0db888df7a77f228ffc4eec612068c45f
RYA234 <mryuabc@gmail.com>
2022-12-31 10:48:14 +0900

    認証　認可まで完了

--- ecshop_front/service/customerService.ts
+++ ecshop_front/service/customerService.ts
@@ -1,9 +1,21 @@
 import axios from 'axios';
+import { URLSearchParams } from 'url';
 import { Customer } from '../types/customer';
 
 
-export const signIn = ({email,password}:Customer) =>{
-	return axios.post(`http://127.0.0.1:5000/api/auth/sigin`);
+
+export const signIn = ({email,password}: Customer) =>{
+	return axios.post(`http://127.0.0.1:5000/api/auth/signin`,
+	{ email: email, password: password },
+	{ headers: { 
+		'Request-Method' : 'POST',
+		'Content-Type': 'application/json',
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Headers': '*',
+		'Access-Control-Allow-Credentials': 'true'
+	} } //COR回避
+		
+	);
 };
 
 export const logout =()=>{
@@ -12,6 +24,10 @@ export const logout =()=>{
 
 
 //認可が必要なapi 本来だったら別カテゴリーに移す
-export const getCartItems =()=>{
-	return axios.get(`http://127.0.0.1:5000/api/cart/all`)
+export const getCartItems =(jwtAccessKey : string)=>{
+	return axios.get(`http://127.0.0.1:5000/api/cart/all`,
+	{ headers : {
+			'Authorization': `Bearer ${jwtAccessKey}`
+		}}
+	);
 }
