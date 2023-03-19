75feb5cc862efb8a77721ae67fcb569b9f1482ff
RYA234 <103587568+RYA234@users.noreply.github.com>
2023-03-19 08:55:38 +0000

    カテゴリーメニュー、ヘッダー、商品表示（ページネーションなし）まで実装

--- ecshop_front/component/header.tsx
+++ ecshop_front/component/header.tsx
@@ -1,20 +1,69 @@
-export default function Header({ left, top, color, bgColor, height, width }: any) {
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Header() {
+	
+	const [isLogin, setIsLogin] = useState(false);
+
+
+
 	return (
 		<div>
 		<header>
-				ヘッダー
+			<div className="box1">logo</div>
+			<div className="btn-area">
+				{isLogin ? 
+				<>
+					<button>ログアウト</button> 
+					<button>マイページ</button>
+									
+				</>
+				: 
+				<>
+				<div className="link">
+					<Link href="/login">新規登録</Link>
+				</div>
+				<div className="link">
+					<Link href="register">ログイン</Link>
+				</div>
+				</>
+			}	
+			</div>
+
+
 		</header>
 			<style jsx>{`
-				div{			
-					// position:fixed;
-					// left:${left}px;
-					// top:${top}px;
-					// width: 100%;
-				    //  height:${height}px;
-					// color:${color};
-					// background:${bgColor};	
-					z-index:2;	
+				header{
+					width:100%;
+					height:100px;
+					background-color:#dcdcdc;
+				  /* display:flexでalign-items:centerにすることで要素を真ん中に配置出来ます   */
+					display:flex;
+					align-items:center;
+					justify-content:space-between;
+				  }
+				.link{
+					background-color:white;
+					border:none;
+					padding:10px 20px;
 				}
+				  .box1{
+					background-color:blue;
+					width:200px;
+					text-align:center;
+					padding:20px 0;
+					color:white;
+				  }
+				  .btn-area{
+					display:inline-flex;
+					margin-right:20px;
+					gap: 6px 4px; /* 余白 */
+				   }
+				  button{
+					background-color:white;
+					border:none;
+					padding:10px 20px;
+				  }
 			`}</style>
 		</div>
 
--- ecshop_front/pages/index.tsx
+++ ecshop_front/pages/index.tsx
@@ -1,17 +1,80 @@
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import Header from '../component/header';
+import { CartItemResponse } from '../types/cartItem/cartItemResponse';
+import { Category } from '../types/category/category';
+import { ProductResponse } from '../types/product/productResponse';
+import * as CategoryService from "../service/categoryService";
+import CategoryMenu from '../component/categoryMenu';
+import * as productServiceV2 from '../service/productServiceV2';
+import ProductView from '../component/product/productView';
+// contextの作成
+export const mainContext  = createContext(0);
 // import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Home() {
-	// ApiFetch();
+
+	
+
+	  // 商品情報のhttpレスポンス
+	  const [productResponse, setProductResponse] = useState<ProductResponse>();
+	  // 買い物カゴ情報のhttpレスポンス
+	  const [cartItemsResponse, setCartItemsResponse] = useState<CartItemResponse>();
+	  // カテゴリー情報のhttpレスポンス
+	  const [categoryResponse,setCategoryResponse] = useState<Category[]>([]);
+	  const[categoryId,setCategoryId] = useState<number>(1);
+	  const [pageNo,setPageNo] = useState<number>(1)
+	  
+	  // １ページあたりの商品数
+	  const pageSize : number = 10;
+
+	  // 初回実行される関数
+	  useEffect(() => {
+		init();
+			},[]);
+
+	// pageNoが変わるとgetProductByCategoryが実行される
+	
+	const init = async ()=>{
+		await getCategory();
+		await getProductByCategory();
+	}
+
+
+	  
+  // カテゴリーの情報を取得する
+    const getCategory = async () => {
+		const response = await CategoryService.getCategories();
+		await setCategoryResponse(response);
+		// console.log(response);
+	  }
+
+   // 商品情報を取得する。
+	  const getProductByCategory = async () => {
+		const response : ProductResponse= await productServiceV2.getProductsByCategoryId2(pageNo - 1,pageSize,categoryId)
+		await setProductResponse(response)
+		console.log(response)
+	}
+
 	return (
 
 		<div className = 'headCenterFooter'>
-			<header>ヘッダー </header>
+			<header>
+				<Header/>
+
+			</header>
 			<main>
 				<div className='leftCenterRightInMain'>
 					<div className="left">
 						左側
+						
+					<mainContext.Provider value={{ categoryResponse,setCategoryId}}>
+						<CategoryMenu/>
+					</mainContext.Provider>
 					</div>
 					<div className='center'>
+					<mainContext.Provider value={{ productResponse, setProductResponse,setCartItemsResponse,categoryId }}>
+						<ProductView />
+					</mainContext.Provider>
+
 						センター
 						<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 						<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
@@ -69,6 +132,11 @@ export default function Home() {
 				.right{
 					flex:1;
 					background:red;
+				}
+
+				.PaginationPosition{
+					margin:auto;
+					width:50%
 				}
 			`}</style>
 		</div>
