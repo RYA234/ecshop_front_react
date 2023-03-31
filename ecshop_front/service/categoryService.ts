70cfd43a9e8a008552b6c1d0b407371b1dcd6483
RYA234 <103587568+RYA234@users.noreply.github.com>
2023-03-31 03:23:16 +0000

    categoryService ヘッダー追加

--- ecshop_front/service/categoryService.ts
+++ ecshop_front/service/categoryService.ts
@@ -3,7 +3,15 @@ import { Category } from '../types/category/category';
 import { CategoryResponse } from '../types/category/categoryResponse';
 
  export  const getCategories = async () =>{
-	return await axios.get(process.env.NEXT_PUBLIC_BACKEND_URL + `/api/category/all`)
+	return await axios.get(process.env.NEXT_PUBLIC_BACKEND_URL + `/api/category/all`,
+	{ headers : {
+		'Request-Method' : 'GET',
+		'Content-Type': 'application/json',
+		'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_BACKEND_URL+'/*',
+		'Access-Control-Allow-Headers': 'accept, accept-language, content-language, content-type',
+		'Access-Control-Allow-Credentials': 'true'
+	}}
+	)
 	.then((response)=>{
 		return response.data as AxiosResponse<Category[]>;
 	})
