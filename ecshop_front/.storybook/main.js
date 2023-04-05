4622a12e4d1b439560b94176b79eaab1a2c0179e
RYA234 <103587568+RYA234@users.noreply.github.com>
2023-04-05 16:58:35 +0000

    aasa

--- ecshop_front/.storybook/main.js
+++ ecshop_front/.storybook/main.js
@@ -1,17 +1,15 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../components/**/*.stor@(y|ies).[tj]sx"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    'storybook-addon-next',
-  ],
-  "framework": "@storybook/react",
-
-
-
-}
+  "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)", "../components/**/*.stor@(y|ies).[tj]sx"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  "framework": {
+    name: "@storybook/nextjs",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  },
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+};
