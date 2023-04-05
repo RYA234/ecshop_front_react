d6719c62e6930a0105bca362b1d01b3de624fcd4
RYA234 <103587568+RYA234@users.noreply.github.com>
2023-04-05 17:22:18 +0000

    dsds

--- ecshop_front/.storybook/main.js
+++ ecshop_front/.storybook/main.js
@@ -2,7 +2,6 @@ module.exports = {
   "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)", "../components/**/*.stor@(y|ies).[tj]sx"],
   "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
   "framework": {
-    name: "@storybook/nextjs",
     options: {}
   },
   docs: {
