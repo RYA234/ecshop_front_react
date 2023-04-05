f3ddac8f563ec38948e6f24d78ca4885aa5fb012
RYA234 <103587568+RYA234@users.noreply.github.com>
2023-04-05 15:22:23 +0000

    再チェック

--- ecshop_front/.storybook/main.js
+++ ecshop_front/.storybook/main.js
@@ -8,11 +8,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-<<<<<<< HEAD
     'storybook-addon-next',
-=======
-    "@storybook/addon-links/register"
->>>>>>> 5eb562ec6a36dc4653f6f11c2c5cfc7c543ed824
   ],
   "framework": "@storybook/react",
   "core": {
