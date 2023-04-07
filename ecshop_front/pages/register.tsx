27cca0477604c333fc81856f2bb014b535785050
RYA234 <103587568+RYA234@users.noreply.github.com>
2023-04-07 09:20:50 +0000

    新規作成

--- ecshop_front/pages/register.tsx
+++ ecshop_front/pages/register.tsx
@@ -25,21 +25,132 @@ export default function Register() {
 	}
 	return(
 	<div className="Layout">
-			<div>ユーザー登録画面</div>
-			入力したEメールに認証メールが送られます。
-			<div>Emailアドレス</div>
-			<input type="text" onChange={onChangeEmailHandler}></input>		
-			<div>パスワード</div>
-			<input type="password" onChange={onChangePasswordHandler}></input>
-			<br/>
-			<br/>
-			<button onClick={onRegisterClick} >ユーザー登録</button>
+			<div className="Title" >会員登録画面</div>
+			<div className="Content">入力したEメールに認証メールが送られます。</div>
+			<div className="EmailText">Eメール</div>
 
-
+			<input type="text" onChange={onChangeEmailHandler} className="InputEmail"></input>		
+			<div className="PasswordText">パスワード</div>
+			<input type="password" onChange={onChangePasswordHandler} className="InputPassword"></input>
+			<br/>
+			<button onClick={onRegisterClick} className="RegisterButton" >
+				ユーザー登録
+			</button>
 		<style jsx>{`
-				.Layout{
-					text-align: center;
+				.Content{
+					position: absolute;
+					height: 29px;
+					width: 488px;
+					left: 476px;
+					top: 151px;
+					border-radius: nullpx;		
+					// font-family: 'Impact';
+					font-style: normal;
+					font-weight: 400;
+					font-size: 24px;
+					line-height: 29px;
+					
+					color: #000000;					
 				}
+				.Layout{
+					// text-align: center;
+				}
+				.RegisterButton{
+					box-sizing: border-box;
+
+					position: absolute;
+					height: 40px;
+					width: 167px;
+					left: 637px;
+					top: 442px;
+					border-radius: 5px;		
+					background: #8DD4FC;
+					border: 1px solid #000000;
+					border-radius: 5px;
+	
+					font-size: 20px
+				}
+				.Title{
+					position: absolute;
+					height: 49px;
+					width: 240px;
+					left: 600px;
+					top: 84px;
+					border-radius: nullpx;
+					
+										
+					font-family: 'Impact';
+					font-style: normal;
+					font-weight: 400;
+					font-size: 40px;
+					line-height: 49px;
+					/* ボックスの高さと同一 */		
+					color: #000000;
+						
+				}
+				.EmailText{
+					position: absolute;
+					height: 49px;
+					width: 145px;
+					left: 449px;
+					top: 217px;
+					border-radius: nullpx;
+							
+					// font-family: 'Impact';
+					font-style: normal;
+					font-weight: 400;
+					font-size: 40px;
+					line-height: 49px;
+					/* ボックスの高さと同一 */
+					color: #000000;
+				}
+				.InputEmail{
+					box-sizing: border-box;
+					position: absolute;					
+					height: 44px;
+					width: 335px;
+					left: 613px;
+					top: 222px;
+					border-radius: 4px;
+					
+					background: #FFFFFF;
+					border: 1px solid #000000;
+					border-radius: 4px;
+					font-size: 30px;	
+				}
+				.PasswordText{
+					position: absolute;
+					height: 49px;
+					width: 209px;
+					left: 385px;
+					top: 327px;
+					border-radius: nullpx;									
+					font-family: 'Impact';
+					font-style: normal;
+					font-weight: 400;
+					font-size: 40px;
+					line-height: 49px;
+					/* ボックスの高さと同一 */			
+					color: #000000;		
+
+				}
+				.InputPassword{
+					/* InputPassword */
+					box-sizing: border-box;
+					
+					position: absolute;
+					height: 44px;
+					width: 335px;
+					left: 613px;
+					top: 330px;
+					border-radius: 4px;			
+					background: #FFFFFF;
+					border: 1px solid #000000;
+					border-radius: 4px;
+					font-size: 30px;								
+				}
+				
+
 			`}</style>
 
 	</div>
