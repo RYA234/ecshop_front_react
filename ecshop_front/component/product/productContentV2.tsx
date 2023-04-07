ee5273b7313bf67dab95c4594b589fd67318c32e
RYA234 <103587568+RYA234@users.noreply.github.com>
2023-04-07 14:43:16 +0000

    商品コンポーネントのUIを改善

--- ecshop_front/component/product/productContentV2.tsx
+++ ecshop_front/component/product/productContentV2.tsx
@@ -60,55 +60,147 @@ export default function ProductContentV2(props : Props){
 
 	return(
 		<>
-			<div className = 'ContentPadding ContentLayout'>
-				<Image src={props.imageURL}
-				width={160}
-				height={150}
-				alt='logo' />
-				<div>{props.productName}</div>
-				<div className='PriceWithoutTaxAndQuantityLayout'>
-					<div>{`税抜` + props.priceWithoutTax  +`円`}</div>
-					<div>数量</div>
-					<div>
-						<select value={tempNum} onChange={(e: React.ChangeEvent<HTMLSelectElement>)=>onChangeHandler(e)}>
-							<option value={tempNum}>{tempNum}</option>
-							<option value="1">1</option>
-							<option value="2">2</option>
-							<option value="3">3</option>
-							<option value="4">4</option>
-							<option value="5">5</option>
-						</select>
-					</div>
+			<div className = 'Layout'>
+				
+				<div className ="Image">
+					<Image   src={props.imageURL}
+					width={160}
+					height={150}
+					alt='logo' />
 				</div>
-				<div className='PriceIncludingTaxAndBuyButtonLayout'>
-				<div>{`税込` + props.priceIncludingTax  +`円`}</div>
-				<button onClick={addProductHandler} >購入</button>
-				</div>
+				<div className="ProductName">{props.productName}</div>
+				<div className='WithoutTax'>{`税抜` + props.priceWithoutTax  +`円`}</div>
+				<div className="Amount">数量</div>
+		
+				<select className="Selector" value={tempNum} onChange={(e: React.ChangeEvent<HTMLSelectElement>)=>onChangeHandler(e)}>
+					<option value={tempNum}>{tempNum}</option>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+				</select>
+				
+			
+				<div className="WithTax">{`税込` + props.priceIncludingTax  +`円`}</div>
+				<button onClick={addProductHandler}  className="Button">カートに入れる</button>
+				
 			</div>
 
 			<style jsx>{`				
 				// 大枠の空白部分を担当
-				.ContentPadding{
-					padding: 20px;
-					border:1px solid black;
+				.Layout{
+					position: relative;
+					width: 200px;
+					height: 258px;	
+					border: 1px solid #000000;				
 				}
-				// 大枠のレイアウト（縦並び）		
-				.ContentLayout{
-					position:relative;
-					flex:1;
-					width:200px;
-					height:350px;
+				.Selector{
+					box-sizing: border-box;
+
+					position: absolute;
+					// width: 23px;
+					// height: 19px;
+					left: 129px;
+					top: 200px;
+					border-radius: 5px;					
+					background:white;
+					border: 1px solid #000000;
 				}
-				// 税抜き価格と数量のレイアウト（横並び-等間隔）
-				.PriceWithoutTaxAndQuantityLayout{
-					display:flex;
-					justify-content:space-between;
+				.Amount{
+					position: absolute;
+					width: 24px;
+					height: 15px;
+					left: 96px;
+					top: 202px;
+					
+					font-family: 'Inter';
+					font-style: normal;
+					font-weight: 100;
+					font-size: 12px;
+					line-height: 15px;
+					/* ボックスの高さと同一 */
+					
+					
+					color: #000000;					
 				}
-				// 税込み価格と購入ボタンのレイアウト（横並び-等間隔）
-				.PriceIncludingTaxAndBuyButtonLayout{
-					display:flex;
-					justify-content:space-between;
+				.ButtonText{
+
 				}
+
+				.Button{
+					box-sizing: border-box;
+
+					position: absolute;
+					width: 87px;
+					height: 20px;
+					left: 100px;
+					top: 226px;
+					
+					background: #EC7777;
+					border: 1px solid #000000;
+					border-radius: 4px;
+					color:white;
+					font-size:8px;
+				}
+				.WithTax{
+					position: absolute;
+					// width: 67px;
+					height: 15px;
+					left: 21px;
+					top: 222px;
+					
+					font-family: 'Inter';
+					font-style: normal;
+					font-weight: 100;
+					font-size: 12px;
+					line-height: 15px;
+					/* ボックスの高さと同一 */
+					
+					
+					color:red;
+				}
+				.WithoutTax{
+					position: absolute;
+					// width: 54px;
+					height: 15px;
+					left: 21px;
+					top: 202px;
+					
+					font-family: 'Inter';
+					font-style: normal;
+					font-weight: 100;
+					font-size: 12px;
+					line-height: 15px;
+					/* ボックスの高さと同一 */
+					
+					
+					color: #000000;
+				}
+				.ProductName{
+					/* Name */
+
+
+					position: absolute;
+					width: 158px;
+					height: 30px;
+					left: 21px;
+					top: 163px;
+					
+					font-family: 'Inter';
+					font-style: normal;
+					font-weight: 100;
+					font-size: 12px;
+					line-height: 15px;
+					
+					color: #000000;
+				}
+				.Image{
+					position: relative;;
+					left: 25px;
+					top: 13px;
+				}
+				
 			`}</style>	
 		</>
 	)
