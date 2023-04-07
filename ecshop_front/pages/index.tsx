8b4e4e486fcd67d35cbba1e2b2e5b41f1fa20f34
RYA234 <103587568+RYA234@users.noreply.github.com>
2023-04-07 09:25:06 +0000

    welcomepage追加

--- ecshop_front/pages/index.tsx
+++ ecshop_front/pages/index.tsx
@@ -1,138 +1,98 @@
-import { createContext, useEffect, useState } from 'react';
-import Header from '../component/header';
-import { CartItemResponse } from '../types/cartItem/cartItemResponse';
-import { Category } from '../types/category/category';
-import { ProductResponse } from '../types/product/productResponse';
-import * as CategoryService from "../service/categoryService";
-import CategoryMenu from '../component/categoryMenu';
-import * as productServiceV2 from '../service/productServiceV2';
-import ProductView from '../component/product/productView';
-import * as CartItemService2 from "../service/cartItemServiceV2";
-import CartItemView from '../component/cartItem/cartItemView';
-import Footer from '../component/footer/footer';
+import Footer from "../component/footer/footer";
+import LoginCard from "../component/welcome/loginCard";
+import RegisterCard from "../component/welcome/registerCard";
+import TrialCard from "../component/welcome/trialCard";
 
-// contextの作成
-export const mainContext  = createContext(0);
-// import 'bootstrap/dist/css/bootstrap.min.css';
-export default function Home() {
-
-	  // 商品情報のhttpレスポンス
-	  const [productResponse, setProductResponse] = useState<ProductResponse>();
-	  // 買い物カゴ情報のhttpレスポンス
-	  const [cartItemsResponse, setCartItemsResponse] = useState<CartItemResponse>();
-	  // カテゴリー情報のhttpレスポンス
-	  const [categoryResponse,setCategoryResponse] = useState<Category[]>([]);
-	  const[categoryId,setCategoryId] = useState<number>(1);
-	 
-	  const [pageNo,setPageNo] = useState<number>(1)
-	  const [isLogin,setIsLogin] = useState<boolean>(false)
-	  
-
-	  // １ページあたりの商品数
-	  const pageSize : number = 10;
-	
-
-
-	  // 初回実行される関数
-	  useEffect(() => {
-		init();
-			},[]);
-
-	  useEffect(()=>{
-		getCustomerInformationFromJwtAccessKey();
-	  },[isLogin])
-	const changeLogins = async () =>{
-	
-	}
-	
-	// ローカルストレージのaccessTokenに値が存在する場合ログイン判定をする。
-	const getCustomerInformationFromJwtAccessKey = async () => {
-		if(localStorage.getItem('accessToken') != null && localStorage.getItem('accessToken') != 'AxiosError: Network Error'){
-			let response = await CartItemService2.getCartItems2(
-				localStorage.getItem('accessToken') as string
-			).then((response) => {
-				setCartItemsResponse(response)
-				setIsLogin(true)
-				console.log(response)})
-			
-		}else{
-			setIsLogin(false);
-			setCartItemsResponse(undefined);
-		}
-	}
-	// pageNoが変わるとgetProductByCategoryが実行される
-	const init = async ()=>{
-		await getCategory();
-		await getProductByCategory();
-	}	  
-  // カテゴリーの情報を取得する
-    const getCategory = async () => {
-		const response = await CategoryService.getCategories();
-		await setCategoryResponse(response);
-		// console.log(response);
-	  }
-
-   // 商品情報を取得する。
-	  const getProductByCategory = async () => {
-		const response : ProductResponse= await productServiceV2.getProductsByCategoryId2(pageNo - 1,pageSize,categoryId)
-		await setProductResponse(response)
-		console.log(response)
-	}
-
-	const getCartItem = async () => {
-		let response: CartItemResponse = await CartItemService2.getCartItems2(
-		  localStorage.getItem("accessToken") as unknown as string
-		);
-		setCartItemsResponse(response);
-		console.log(response);
-	  };
-	console.log(process.env.TEST);
-	return (
-		<div className = 'headCenterFooter'>
-			<header>	
-				<mainContext.Provider value={{isLogin,setIsLogin}}>
-				<Header/>
-				</mainContext.Provider>
-			</header>
-		
-			<div className='leftCenterRightInMain'>
-				<div className="left">
-				<mainContext.Provider value={{ categoryResponse,setCategoryId}}>
-					<CategoryMenu/>
-				</mainContext.Provider>
-			</div>
-			<div className='center'>
-				<mainContext.Provider value={{ productResponse, setProductResponse,setCartItemsResponse,categoryId }}>
-					<ProductView />
-				</mainContext.Provider>
-			</div>
-
-				<div className="right">
-					右側
-					{(cartItemsResponse == undefined) ? <div></div>
-					:
-					<mainContext.Provider value={{ cartItemsResponse, setCartItemsResponse }}>
-						<CartItemView />
-					</mainContext.Provider>
-					}
+ export default  function Index(){
+    return(
+       <div className = 'headCenterFooter'>
+         <header>
+            ヘッダ
+         </header>
+		 <div className='leftCenterRightInMain'>
+				<div className="TitleBack">
+					<div className="TitleText">ネットスーパーへようこそ</div>
 				</div>
-			</div>
+				<div className="CardBack"></div>
+				<div className="RegisterPosition">
+					<RegisterCard/>
+				</div>
+				<div className="TrialPosition">
+					<TrialCard/>
+				</div>
+				<div className="LoginPosition">
+					<LoginCard/>
+				</div>
+		  </div>         
+         <footer><Footer/></footer>
 
-			<footer><Footer/></footer>
-		
 			<style jsx>{`
+				.TitleBack{
+					position: absolute;
+					width: 1208px;
+					height: 44px;
+					left: 116px;
+					top: 185px;
+					background: #1BE1B2;
+					border-radius: 3px;					
+				}
+				.TitleText{
+					font-family: 'Inter';
+					font-style: normal;
+					font-weight: 700;
+					font-size: 15px;
+					position: absolute;
+					width: 195px;
+					height: 18px;
+					left: 18px;
+					top: 14px;
+					color: #FFFAFA;
+				}
+				.CardBack{
+					position: absolute;
+					width: 1208px;
+					height: 240px;
+					left: 116px;
+					top: 244px;
+					background: #D9D9D9;
+					border-radius: 8px;
+				}
+				.RegisterPosition{
+					box-sizing: border-box;
+					position: absolute;
+					width: 392px;
+					height: 224px;
+					left: 125px;
+					top: 252px;
+					border: 1px solid #000000;
+				}
+				.TrialPosition{
+					box-sizing: border-box;
+					position: absolute;
+					width: 392px;
+					height: 224px;
+					left: 524px;
+					top: 252px;
+					border: 1px solid #000000;
+				}
+				.LoginPosition{
+					box-sizing: border-box;
+					position: absolute;
+					width: 392px;
+					height: 224px;
+					left: 923px;
+					top: 252px;					
+					border: 1px solid #000000;
+					filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+				}
 				.headCenterFooter{
 					display: flex;
 					flex-direction: column;		
 					margin: 0;
 					min-height: 100vh;							
 				}
-
 				header{
-					// position:sticky;
-					// top: 0;
-					// left: 0;
-					width: 100%;
+					width: 1440px;
 					height: 120px;
 					color:black;
 					background:yellow;
@@ -142,9 +102,6 @@ export default function Home() {
 					width: 1440px;
 					height: 178px;
 					left: calc(50% - 1440px/2);
-					// position: fixed;/*←絶対位置*/
-					// bottom: 0; /*下に固定*/
-					// margin-top: auto;
 					border: 1px solid #1BE1B2;
 				}
 				main{
@@ -153,11 +110,9 @@ export default function Home() {
 				}
 				.leftCenterRightInMain{
 					display: flex;
-					// flex-direction: row;
 					flex:1
 				}
 				.left{
-					// flex:0.5;
 					color:rainbow;
 					background:white;
 					width:160px;
@@ -167,7 +122,6 @@ export default function Home() {
 					background:white;
 				}
 				.right{
-					// flex:1;
 					background:white;
 				}
 
@@ -176,17 +130,6 @@ export default function Home() {
 					width:50%
 				}
 			`}</style>
-		</div>
-	)
-};
-// restapi 検証用の関数。
-export const ApiFetch = () => {
-	const [stones, setStone] = useState([]);
-
-	useEffect(() => {
-		// APIをfetchする(呼び出す)
-		fetch("http://localhost:5000/api/products", { method: "GET" })
-			// レスポンスのデータ形式をjsonに設定
-			.then((res) => res.json())
-	}, []);
+       </div> 
+    )
 }
