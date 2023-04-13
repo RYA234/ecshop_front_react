4e7da6a37a6c4e4cd973bd61e7e3197876f4d4a1
RYA234 <103587568+RYA234@users.noreply.github.com>
2023-04-13 08:05:02 +0000

    globalCssを追加　レイアウト

--- ecshop_front/pages/_app.tsx
+++ ecshop_front/pages/_app.tsx
@@ -1,15 +1,48 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise =  loadStripe('pk_test_51MdNNUEJO02RSJ5FUgM60DZSqfZLgfVZTLB1JsCFKhFgPXGJUzp7vBhAOovUXxLQ2SoU7ykWvPN46SY2zJsOms1900UW3EV2qB');
+const stripePromise = loadStripe(
+  "pk_test_51MdNNUEJO02RSJ5FUgM60DZSqfZLgfVZTLB1JsCFKhFgPXGJUzp7vBhAOovUXxLQ2SoU7ykWvPN46SY2zJsOms1900UW3EV2qB"
+);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return( 
-  <> <Elements stripe={stripePromise} >
-  		    <Component {...pageProps} />
+  return (
+    <>
+      {" "}
+      <Elements stripe={stripePromise}>
+        <Component {...pageProps} />
       </Elements>
-  </>
-  )
+      <style jsx global>{`
+        // 共通レイアウト部分(Header Main Footer）
+        header {
+          width: 1440px;
+          height: 120px;
+          color: black;
+          background: white;
+          z-index: 100;
+        }
+        footer {
+          width: 1440px;
+          height: 178px;
+          left: calc(50% - 1440px / 2);
+          border: 1px solid #1be1b2;;
+		  flex:1;
+		  position:relative;
+		  top:120px;
+
+        }
+        .Main {
+          flex: 1;
+        }
+        .HeadCenterFooter {
+          display: flex;
+          flex-direction: column;
+          margin: 0;
+          min-height: 100vh;
+        }
+      `}</style>
+    </>
+  );
 }
