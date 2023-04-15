11a47051806f15e9435c3b196f075a16af40f11b
RYA234 <103587568+RYA234@users.noreply.github.com>
2023-04-15 09:10:34 +0000

    アイコンを大きくする

--- ecshop_front/component/footer/footerIcon.tsx
+++ ecshop_front/component/footer/footerIcon.tsx
@@ -3,93 +3,135 @@ import Image from 'next/image';
 
 export default function FooterIcon(){
     let storybookURL = process.env.NEXT_PUBLIC_FRONTEND_URL + ":6006"
+    const iconSize :number = 48;
 
     return(
         <div>
             <div className="TitleBack"></div>
             <div className="TitleText">リンク類</div>
 
-            <div className='FrontEndTextStyle'>フロントエンド</div>
+           
             <div className='FrontEndGithubIcon'>
                 <a href="https://github.com/RYA234/ecshop_front_react" target="_blank" rel="noreferrer">
                     <Image src='/icons/GithubIcon.JPG'
-                        width={16} 
-                        height={16}
+                        width={iconSize} 
+                        height={iconSize}
                         alt='logo' />
                 </a>
             </div>
-
+            <div className='FrontEndGithubText'>フロントエンドgithub</div>
+            
             <div className='StoryBookIcon'>
                 <a href={storybookURL} target="_blank" rel="noreferrer" >
                     <Image src='/icons/StoryBookIcon.JPG'
-                        width={16} 
-                        height={16}
+                        width={iconSize} 
+                        height={iconSize}
                         alt='logo' />
                 </a>
             </div>
-            <div className='BackEndTextStyle'>バックエンド</div>
+            <div className='StoryBookText'>Storybook</div>
+            
+
             <div className='BackEndGithubIcon'>
                 <a href="https://github.com/RYA234/springboot-ecshop-rest-api" target="_blank" rel="noreferrer">
                     <Image src='/icons/GithubIcon.JPG'
-                        width={16} 
-                        height={16}
+                        width={iconSize} 
+                        height={iconSize}
                         alt='logo' />
                 </a>
             </div>
-
+            <div className='BackEndGithubText'>バックエンドgithub</div>
             <div className='SwaggerUIIcon'>
                 <a href="https://ecshoprest-backend.com/swagger-ui/" target="_blank" rel="noreferrer">
                     <Image src='/icons/SwaggerUIIcon.JPG'
-                        width={16} 
-                        height={16}
+                        width={iconSize} 
+                        height={iconSize}
                         alt='logo' />
                 </a>
             </div>
-
-            <div className='OtherTextStyle'>その他</div>
+            <div className='SwaggerText'>SwaggerUI</div>
+        
             <div className='QiitaIcon'>
                 <a href="https://qiita.com/RYA234/items/8dcb1ce01852ebb03afd" target="_blank" rel="noreferrer">
                     <Image src='/icons/QiitaIcon.JPG'
-                        width={16} 
-                        height={16}
+                        width={iconSize} 
+                        height={iconSize}
                         alt='logo' />
                 </a>
             </div>
+            <div className='QiitaText'>Qiitaでの記録</div>
 
 
 			<style jsx>{`
             .FrontEndTextStyle{
-                /* FrontText */
                 position: absolute;
-                height: 10px;
-                left: -2px;
-                top: 30px;
-                
+                width: 121px;
+                height: 15px;
+                left: 6px;
+                top: 72px;
                 
                 font-family: 'Inter';
                 font-style: normal;
                 font-weight: 400;
-                font-size: 8px;
-                line-height: 10px;
-                /* ボックスの高さと同一 */                
-                color: #000000;                                
-            }				
+                font-size: 12px;
+                line-height: 15px;
+                /* ボックスの高さと同一 */
+                
+                
+                color: #000000;                              
+            }	
+            .FrontEndGithubText{
+                position: absolute;
+                width: 121px;
+                height: 15px;
+                left: 6px;
+                top: 72px;
+                
+                font-family: 'Inter';
+                font-style: normal;
+                font-weight: 400;
+                font-size: 12px;
+                line-height: 15px;
+                /* ボックスの高さと同一 */
+                
+                
+                color: #000000;    
+            }			
             .FrontEndGithubIcon{
                 position: absolute;
-                width: 15.06px;
-                height: 16px;
-                left: -2px;
-                top: 40px;                
+                width: 48px;
+                height: 48px;
+                left: 41px;
+                top: 24px;
+                             
+            }
+            .StoryBookText{
+                position: absolute;
+                width: 60px;
+                height: 15px;
+                left: 141px;
+                top: 72px;
+                
+                font-family: 'Inter';
+                font-style: normal;
+                font-weight: 400;
+                font-size: 12px;
+                line-height: 15px;
+                /* ボックスの高さと同一 */
+                
+                
+                color: #000000;
+                
             }
             .StoryBookIcon{
                 position: absolute;
-                width: 15.06px;
-                height: 16px;
-                left: 15px;
-                top: 40px;                
+                width: 48px;
+                height: 48px;
+                left: 149px;
+                top: 24px;              
             }
 
-            .BackEndTextStyle{
+            .BackEndGithubText{
                 /* BackText */
                 position: absolute;
                 width: 65px;
@@ -106,17 +148,49 @@ export default function FooterIcon(){
             }
             .BackEndGithubIcon{
                 position: absolute;
-                width: 16px;
-                height: 17px;
-                left: -2px;
-                top: 70px;                
+                width: 48px;
+                height: 48px;
+                left: 43px;
+                top: 98px;               
+            }
+            .BackEndGithubText{
+                position: absolute;
+                width: 109px;
+                height: 15px;
+                left: 6px;
+                top: 144px;
+                
+                font-family: 'Inter';
+                font-style: normal;
+                font-weight: 400;
+                font-size: 12px;
+                line-height: 15px;
+                /* ボックスの高さと同一 */
+                
+                
+                color: #000000;                
             }
             .SwaggerUIIcon{
                 position: absolute;
-                width: 16px;
-                height: 16px;
-                left: 15px;
-                top: 70px;
+                width: 48px;
+                height: 48px;
+                left: 149px;
+                top: 98px;
+            }
+            .SwaggerText{
+                position: absolute;
+                width: 65px;
+                height: 10px;
+                left: 139px;
+                top: 146px;
+                
+                font-family: 'Inter';
+                font-style: normal;
+                font-weight: 400;
+                font-size: 12px;
+                line-height: 15px;
+                
+                color: #000000;               
             }
 
             .OtherTextStyle{
@@ -141,12 +215,26 @@ export default function FooterIcon(){
             }
 
             .QiitaIcon{
-                /* QiitaIcon */
                 position: absolute;
-                width: 16px;
-                height: 16px;
-                left: -2px;
-                top: 103px;                
+                width: 48px;
+                height: 48px;
+                left: 251px;
+                top: 98px;               
+            }
+            .QiitaText{
+                position: absolute;
+                width: 82px;
+                height: 10px;
+                left: 234px;
+                top: 145px;
+                
+                font-family: 'Inter';
+                font-style: normal;
+                font-weight: 400;
+                font-size: 12px;
+                line-height: 15px;
+                
+                color: #000000;                
             }
             .TitleBack{
                 /* タイトルの背景 */
